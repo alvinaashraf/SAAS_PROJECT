aeb5a0b84c22eb5b2d842cae96506c5b529ad406
alvinaashraf <alvina1409b@aptechgdn.net>
2024-06-17 16:25:29 +0500

    second commit

--- src/saashome/settings.py
+++ src/saashome/settings.py
@@ -25,8 +25,12 @@ SECRET_KEY = 'django-insecure-(^qx953+)%b-w7-@k%hit39)jk7%4@yaq1h!nxeyaex=!whgri
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [".railway.app"]
+if DEBUG:
+    ALLOWED_HOSTS +=[
+        "127.0.0.1",
+        "localhost"
+    ]
 
 # Application definition
 
