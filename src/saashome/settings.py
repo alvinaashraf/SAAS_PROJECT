bedaad0a939890df7104cef205bba09104208c77
alvinaashraf <alvina1409b@aptechgdn.net>
2024-06-17 20:37:45 +0500

    debug check

--- src/saashome/settings.py
+++ src/saashome/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,8 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(^qx953+)%b-w7-@k%hit39)jk7%4@yaq1h!nxeyaex=!whgri'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower()=="true"
 
+print("debug",DEBUG, type(DEBUG))
 ALLOWED_HOSTS = [".railway.app"]
 if DEBUG:
     ALLOWED_HOSTS +=[
